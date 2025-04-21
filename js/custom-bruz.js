f532ce163f6a722738ca0ae912e329703bbf7440
aeberling <adamgenweb@gmail.com>
2025-04-21 13:06:08 -0600

    Header/Nav improve, remove checkout pages.

--- js/custom-bruz.js
+++ js/custom-bruz.js
@@ -2,20 +2,49 @@
 
 // Ensure logo is properly sized and positioned when page loads
 $(document).ready(function() {
-    // Adjust logo size and position
-    adjustLogo();
+    // Create a clone of the header for sticky navigation
+    var $headerArea = $('.header-area');
+    var $stickyHeader = $headerArea.clone().addClass('banner--clone').appendTo('body');
     
-    // Adjust logo when window is resized
-    $(window).resize(function() {
-        adjustLogo();
+    // Remove unnecessary elements from sticky header
+    $stickyHeader.find('.social-container').remove();
+    
+    // Adjust only the sticky header logo size
+    $stickyHeader.find('.logo img').css({
+        'max-height': '60px',
+        'width': 'auto',
+        'display': 'block',
+        'padding': '0 10px',
+        'margin': '0'
     });
     
-    function adjustLogo() {
-        // Make sure logo stays in top left and at proper size
-        $('.logo img').css({
-            'max-height': '60px',
-            'width': 'auto',
-            'display': 'block'
-        });
-    }
+    // Adjust the sticky nav vertical spacing
+    $stickyHeader.find('.navigation-container').css({
+        'padding': '0',
+        'margin': '0',
+        'height': 'auto'
+    });
+    
+    $stickyHeader.find('.nav').css({
+        'padding': '0',
+        'margin': '0'
+    });
+    
+    // Handle scroll events
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $stickyHeader.addClass('banner--stick');
+        } else {
+            $stickyHeader.removeClass('banner--stick');
+        }
+    });
+    
+    // Mobile menu adjustments
+    $('.navbar-toggle').on('click', function() {
+        if ($(this).hasClass('collapsed')) {
+            $(this).removeClass('collapsed');
+        } else {
+            $(this).addClass('collapsed');
+        }
+    });
 }); 
